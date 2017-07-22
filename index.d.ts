1ff019c5e461362bd2044726bac7c81136b0df3b
Olegzandr VD <alex@oresoftware.com>
2017-07-22 15:23:39 -0700

    pdev:set

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,5 @@
-export declare const freezeExistingProps: (obj: any) => any;
+export interface IFEPStdObj {
+    [key: string]: any;
+}
+export declare const freezeExistingProps: ($obj: any, depth?: number) => any;
+export declare const freezeAllProps: ($obj: Object, depth?: number) => Object;
--- index.ts
+++ index.ts
@@ -1,20 +1,74 @@
-
 'use strict';
+
+import assert = require('assert');
 
 ////////////////////////////////////
 
-export const freezeExistingProps = function (obj: any) {
+export interface IFEPStdObj {
+  [key: string]: any
+}
 
-  try {
+export const freezeExistingProps = function ($obj: any, depth?: number) {
+
+  depth = depth || 0;
+  assert(Number.isInteger(depth), 'depth value must be an integer.');
+  depth = Math.min(5, depth); // we freeze no more than 5 levels deep
+
+  (function recurse(obj: IFEPStdObj, depth: number) {
+
+    // use try/finally block
+
     Object.keys(obj).forEach(function (key) {
-      Object.defineProperty(obj, key, {
-        writable: false
-      });
+
+      try {
+        Object.defineProperty(obj, key, {
+          writable: false
+        });
+      }
+      catch (e) {
+        console.error(e.stack || e);
+      }
+
+      //depth first freezing
+
+      if (depth > 0 && obj[key]) {
+        recurse(obj[key], --depth);
+      }
+
     });
-  }
 
-  catch (err) {}
+  })($obj, depth);
 
-  return obj;
+  return $obj;
+
+};
+
+export const freezeAllProps = function ($obj: Object, depth?: number) {
+
+  depth = depth || 0;
+  assert(Number.isInteger(depth), 'depth value must be an integer.');
+  depth = Math.min(5, depth); // we freeze no more than 5 levels deep
+
+  (function recurse(obj: IFEPStdObj, depth: number) {
+
+    try {
+      Object.freeze(obj);
+    }
+    catch (e) {
+      console.error(e.stack || e);
+    }
+    finally {
+      Object.keys(obj).forEach(function (key) {
+        //depth first freezing
+        if (depth > 0 && obj[key]) {
+          recurse(obj[key], --depth);
+        }
+
+      });
+    }
+
+  })($obj, depth);
+
+  return $obj;
 
 };
